a6f8853bebed714a4a126892750ee715a4561c48
Golokhvastov Dmitry <dargon89@mail.ru>
2020-07-11 18:43:55 +0700

    Исправляет ошибку в подключении redux-devtools в index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {reducer} from "./reducer.js";
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ : (f) => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
 );
 
 ReactDOM.render(
